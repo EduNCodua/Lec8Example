bc083fc7b71b4b1d1bed1650e6da2b7198b96cd0
oleksandr <drnewman@mail.ru>
2017-05-10 21:53:15 +0300

    v2

--- src/com/netcracker/edu/main/CallableSample.java
+++ src/com/netcracker/edu/main/CallableSample.java
@@ -4,13 +4,10 @@ package com.netcracker.edu.main;
  * Created by admin on 10.05.2017.
  */
 public class CallableSample implements java.util.concurrent.Callable<String> {
-//    @Override
-//    public void run() {
-//
-//    }
 
     @Override
     public String call() throws Exception {
         return Thread.currentThread().toString();
     }
+
 }
